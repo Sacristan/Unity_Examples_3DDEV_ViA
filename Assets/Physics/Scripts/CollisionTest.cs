0832ce838b0c21e3666635c21d039d26b88b5cb5
Sacristan <girts.kesteris@gmail.com>
2017-05-07 14:35:54 +0300

    Added Physics example

---
+++ Assets/Physics/Scripts/CollisionTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTest : MonoBehaviour {
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Just collided with "+collision.collider.gameObject.name);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject colliderGameObject = collider.gameObject;
+        print("Just triggered destroy in 3 seconds for " + colliderGameObject.name);
+        Destroy(colliderGameObject, 3);
+    }
+}
---
+++ Assets/Physics/Scripts/RaycastTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastTest : MonoBehaviour
+{
+    void Update()
+    {
+        RaycastHit hit;
+        
+        Vector3 source = transform.position;
+        Vector3 target = transform.forward;
+
+        Vector3 debugTarget;
+
+        if (Physics.Raycast(source, target, out hit))
+        {
+            debugTarget = hit.point;
+            print("There is an obstacle in front of me!");
+        }
+        else
+        {
+            debugTarget = target;
+            print("There is no obstacle ahead of me!");
+        }
+
+        Debug.DrawLine(source, debugTarget, Color.blue);
+            
+    }
+}
