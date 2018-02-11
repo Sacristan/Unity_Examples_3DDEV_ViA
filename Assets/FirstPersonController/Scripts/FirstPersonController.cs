078c2662835774458d3a6a76a32a4c3073d4195f
Sacristan <girts.kesteris@gmail.com>
2018-02-11 21:07:30 +0200

    add audial example

---
+++ Assets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    const float PitchLimit = 89;
+
+    private float movementSpeed = 50f;
+    private float rotationSpeed = 150f;
+
+    private CharacterController _characterController;
+
+    private float pitch;
+    private float yaw;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        yaw += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+        pitch -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotationSpeed;
+
+        if (pitch > PitchLimit) pitch = PitchLimit;
+        if (pitch < -PitchLimit) pitch = -PitchLimit;
+
+        Vector3 movementVector = new Vector3(
+            hInput,
+            Physics.gravity.y,
+            vInput
+        );
+
+        movementVector = Quaternion.Euler(Vector3.up * yaw) * movementVector;
+        movementVector *= Time.deltaTime * movementSpeed;
+
+        Rotate();
+        Move(movementVector);
+    }
+
+    private void Move(Vector3 movementVector)
+    {
+        _characterController.Move(movementVector);
+    }
+
+    private void Rotate()
+    {
+        transform.localRotation = Quaternion.Euler(pitch, yaw, 0);
+    }
+
+}
---
+++ Assets/TorchController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class TorchController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] flashLightSounds;
+
+    [SerializeField]
+    private Light flashLight;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ToggleFlashLight();
+        }
+    }
+
+
+    private void ToggleFlashLight()
+    {
+        int audioClipIndex = Random.Range(0, flashLightSounds.Length);
+
+        AudioClip flashLightSFX = flashLightSounds[audioClipIndex];
+
+        if (flashLightSFX != null) _audioSource.PlayOneShot(flashLightSFX);
+
+        flashLight.enabled = !flashLight.enabled;
+    }
+
+}
