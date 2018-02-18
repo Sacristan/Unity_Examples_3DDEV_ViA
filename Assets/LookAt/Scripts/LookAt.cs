bf1a61d47817c0becb2accb121eeb53213d17248
Sacristan <girts.kesteris@gmail.com>
2018-02-18 13:40:57 +0200

    add look at example

---
+++ Assets/LookAt/Scripts/LookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
