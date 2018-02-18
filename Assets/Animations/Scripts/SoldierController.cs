c5d42fee73933cd4e8217ffbe6bac71d310d5627
Sacristan <girts.kesteris@gmail.com>
2018-02-18 16:39:23 +0200

    add animation from keyframes scene example

---
+++ Assets/Animations/Scripts/SoldierController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 5f;
+
+	private Transform _playerTransform;
+
+	private CharacterController _characterController;
+
+    private IEnumerator Start()
+    {
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        _characterController = GetComponent<CharacterController>();
+
+        yield return new WaitForSeconds(2f);
+
+        StartCoroutine(AttackPlayer());
+    }
+
+    private IEnumerator AttackPlayer()
+    {
+        GetComponent<Animator>().SetBool("IsAttacking", true);
+
+        while(true)
+        {
+            Vector3 direction = _playerTransform.position - transform.position;
+            direction.Normalize();
+
+            Vector3 deltaMovementVector = direction * Time.deltaTime * speed;
+
+            transform.position += deltaMovementVector;
+            transform.LookAt(_playerTransform);
+
+            yield return null;
+        }
+    }
+
+}
