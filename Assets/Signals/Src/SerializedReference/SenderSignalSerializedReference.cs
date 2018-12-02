95d8ac8d8f50071a66711a9512f52fdb3a2a305b
Sacristan <girts.kesteris@gmail.com>
2018-12-02 15:57:03 +0200

    add signal w serialized ref

---
+++ Assets/Signals/Src/SerializedReference/SenderSignalSerializedReference.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenderSignalSerializedReference : MonoBehaviour
+{
+	[SerializeField] private ReceiverSignalSerializedReference receiverSignalSerializedReference;
+
+    IEnumerator Start()
+    {
+        Debug.Log("SenderSignalSerializedReference Waiting for 1 second...");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("SenderSignalSerializedReference Sending event...");
+        receiverSignalSerializedReference.LaunchEvent();
+    }
+
+}
