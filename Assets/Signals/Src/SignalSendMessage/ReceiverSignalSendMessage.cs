9feb8d79023c24e4a6735e9261fcaeeeacfb5580
Sacristan <girts.kesteris@gmail.com>
2018-12-02 16:45:04 +0200

    add signal w send message

---
+++ Assets/Signals/Src/SignalSendMessage/ReceiverSignalSendMessage.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiverSignalSendMessage : MonoBehaviour
+{
+    public void LaunchEvent()
+    {
+    	Debug.Log("ReceiverSignalSendMessage Received EVENT!");
+    }
+}
---
+++ Assets/Signals/Src/SignalSendMessage/SenderSignalSendMessage.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenderSignalSendMessage : MonoBehaviour
+{
+
+    IEnumerator Start()
+    {
+        Debug.Log("SenderSignalSendMessage Waiting for 1 second...");
+        yield return new WaitForSeconds(1f);
+        Debug.Log("SenderSignalSendMessage Sending event...");
+        GameObject.Find("Receiver").SendMessage("LaunchEvent");
+    }
+
+}
