940cc4b9791408f5eeda2b15b4b7e9a2b2f5ec98
Sacristan <girts.kesteris@gmail.com>
2018-12-02 15:09:13 +0200

    add inverse lerp example

---
+++ Assets/Interpolations/Scripts/InverseLinearInterpolation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseLinearInterpolation : MonoBehaviour
+{
+    [SerializeField] Transform a;
+    [SerializeField] Transform b;
+
+
+    private void Start()
+    {
+        float ax = a.position.x;
+        float bx = b.position.x;
+        float cx = transform.position.x;
+
+        float t = Mathf.InverseLerp(ax, bx, cx);
+        Debug.LogFormat("Cx:{0} progress percentage between Ax: {1} Bx {2} IS -> {3}", cx, ax, bx, t);
+    }
+
+}
