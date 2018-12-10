d63ddff6bfbfa7838294e4d5c0645ed31b601e3b
Sacristan <girts.kesteris@gmail.com>
2018-12-10 13:41:52 +0200

    add 3d object event system demo

---
+++ Assets/UI/Src/UI_ObjectSelection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_ObjectSelection : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    Renderer _renderer;
+
+    Color selectedColor = Color.green;
+    Color unselectedColor = Color.white;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+		_renderer.material.color = selectedColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+		_renderer.material.color = unselectedColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        transform.Rotate(Vector3.up * 45);
+    }
+}
