1cea84d5b4d965cf23d3276209bc664b9ec35d51
Sacristan <girts.kesteris@gmail.com>
2020-03-06 14:48:43 +0200

    lightmap settings

---
+++ Assets/InstancedMaterial.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstancedMaterial : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1f);
+    }
+
+}
