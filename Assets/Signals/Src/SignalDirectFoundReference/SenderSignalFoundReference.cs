2b6605d164622fa12b290232cdd0290971b119c5
Sacristan <girts.kesteris@gmail.com>
2020-04-04 19:39:12 +0300

    fix SenderSignalFoundReference finding ReceiverSignalFoundReference in awake

--- Assets/Signals/Src/SignalDirectFoundReference/SenderSignalFoundReference.cs
+++ Assets/Signals/Src/SignalDirectFoundReference/SenderSignalFoundReference.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class SenderSignalFoundReference : MonoBehaviour
 {
-	private ReceiverSignalFoundReference _receiverSignalFoundReference;
-
-    private void Awake()
-    {
-        _receiverSignalFoundReference = FindObjectOfType<ReceiverSignalFoundReference>();
-    }
-
     IEnumerator Start()
     {
+        ReceiverSignalFoundReference receiverSignalFoundReference = FindObjectOfType<ReceiverSignalFoundReference>();
+
         Debug.Log("SenderSignalFoundReference Waiting for 1 second...");
         yield return new WaitForSeconds(1f);
         Debug.Log("SenderSignalFoundReference Sending event...");
-        _receiverSignalFoundReference.LaunchEvent();
+        receiverSignalFoundReference.LaunchEvent();
     }
 
 }
