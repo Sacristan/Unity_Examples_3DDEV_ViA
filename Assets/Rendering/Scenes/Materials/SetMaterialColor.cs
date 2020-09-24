3109be788060d423769e593a905e036c89c18ea6
Sacristan <girts.kesteris@gmail.com>
2020-09-24 23:05:06 +0300

    material examples

---
+++ Assets/Rendering/Scenes/Materials/SetMaterialColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMaterialColor : MonoBehaviour
+{
+    [SerializeField] Color colorToSet;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = colorToSet;
+    }
+
+}
