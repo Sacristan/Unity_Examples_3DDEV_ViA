0baef0d54cdeaca97533aace86264714dba4a060
Sacristan <girts.kesteris@gmail.com>
2020-10-02 18:33:39 +0300

    continous forces examples

---
+++ Assets/Physics/Scripts/ContinousForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinousForce : MonoBehaviour
+{
+    [SerializeField] ForceMode forceMode = ForceMode.Force;
+    [SerializeField] float movementForce = 5f; //5 newtons per second
+
+    Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(Vector3.right * movementForce, forceMode);
+    }
+}
