244f1248d24e3f202a7d0269718d991e7fbe0a19
Sacristan <girts.kesteris@gmail.com>
2020-10-02 22:50:17 +0300

    add animation event example

---
+++ Assets/AnimationEventCatcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventCatcher : MonoBehaviour
+{
+    public void DidLightMeleeAttack()
+    {
+        Debug.Log("DidLightMeleeAttack");
+    }
+
+    public void DidHeavyMeleeAttack()
+    {
+        Debug.Log("DidHeavyMeleeAttack");
+    }
+}
