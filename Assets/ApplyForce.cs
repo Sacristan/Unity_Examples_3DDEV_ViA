e82414c604cfb90ea6a8c89db1550dcf0f075545
Sacristan <girts.kesteris@gmail.com>
2020-10-02 18:25:02 +0300

    add force mode impulse / velocity change examples

---
+++ Assets/ApplyForce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    [SerializeField] Rigidbody[] rigidbodies;
+    [SerializeField] float forceStrength = 20f;
+
+    [SerializeField] ForceMode forceMode = ForceMode.Impulse;
+
+    private void Start()
+    {
+        StartCoroutine(ApplyForces());
+    }
+
+    IEnumerator ApplyForces()
+    {
+        yield return new WaitForSeconds(2f);
+
+        foreach (Rigidbody rigidbody in rigidbodies)
+        {
+            rigidbody.AddForce(RandomDir() * forceStrength, forceMode);
+        }
+    }
+
+    private Vector3 RandomDir()
+    {
+        return new Vector3(
+            (Random.value > 0.5f ? 1 : -1),
+            (Random.value > 0.5f ? 1 : -1),
+            (Random.value > 0.5f ? 1 : -1)
+        );
+    }
+}
