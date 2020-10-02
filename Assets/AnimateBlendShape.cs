7c59afe0f8ea89e78b5a53ef168b80c274a3adc0
Sacristan <girts.kesteris@gmail.com>
2020-10-02 23:07:30 +0300

    add blendshape example

---
+++ Assets/AnimateBlendShape.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateBlendShape : MonoBehaviour
+{
+    float animationSpeed = 100f;
+
+    SkinnedMeshRenderer _skinnedMeshRenderer;
+
+    private void Start()
+    {
+        _skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    void Update()
+    {
+        _skinnedMeshRenderer.SetBlendShapeWeight(0, Mathf.PingPong(Time.time * animationSpeed, 100f));
+    }
+}
