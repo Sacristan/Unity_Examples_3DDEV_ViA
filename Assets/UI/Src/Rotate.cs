192e2f5def4969bf4d86fd9e7e40c4c27da4247c
Sacristan <girts.kesteris@gmail.com>
2020-10-03 22:00:49 +0300

    screenspace camera example

---
+++ Assets/UI/Src/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed, Space.World);
+    }
+}
