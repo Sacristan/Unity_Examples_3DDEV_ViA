986afc3ea3ca4d09abfecd771d5b6405b376823c
Sacristan <girts.kesteris@gmail.com>
2020-10-03 23:50:56 +0300

    add webrequest example

---
+++ Assets/LoadImageFromWebRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class LoadImageFromWebRequest : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(FetchViaTexture());
+    }
+
+    IEnumerator FetchViaTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture("https://pbs.twimg.com/profile_images/877787847247503360/DHVlmPfz.jpg");
+
+        Debug.Log("Downloading...");
+        yield return www.SendWebRequest();
+
+        Debug.Log("Done");
+
+        Texture2D downloadedTexture = DownloadHandlerTexture.GetContent(www);
+        Sprite sprite = Sprite.Create(downloadedTexture, new Rect(0.0f, 0.0f, downloadedTexture.width, downloadedTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+
+        GetComponent<Image>().sprite = sprite;
+    }
+}
